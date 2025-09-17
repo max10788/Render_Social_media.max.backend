fe14953c0b881490c86b862168bec4be506fcee6
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-17 21:10:59 +0200

    Create init.py

---
+++ app/core/backend_crypto_tracker/blockchain/providers/init.py
@@ -0,0 +1,13 @@
+"""
+API providers for fetching blockchain data.
+"""
+
+from .base_provider import BaseAPIProvider
+from .coingecko_provider import CoinGeckoProvider
+from .binance_provider import BinanceProvider
+from .cryptocompare_provider import CryptoCompareProvider
+from .kraken_provider import KrakenProvider
+from .bitquery_provider import BitqueryProvider
+from .coinmarketcap_provider import CoinMarketCapProvider
+from .bitget_provider import BitgetProvider
+from .coinbase_provider import CoinbaseProvider
