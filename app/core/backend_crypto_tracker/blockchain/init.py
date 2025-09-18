bedff35d81553ce558e0c80e0fa97095f1ab292b
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-18 22:27:25 +0200

    Update init.py

--- app/core/backend_crypto_tracker/blockchain/init.py
+++ app/core/backend_crypto_tracker/blockchain/init.py
@@ -3,19 +3,19 @@ Blockchain module for handling all blockchain-related operations.
 """
 
 # Import all providers
-from .providers.base_provider import BaseAPIProvider
-from .providers.coingecko_provider import CoinGeckoProvider
-from .providers.binance_provider import BinanceProvider
-from .providers.cryptocompare_provider import CryptoCompareProvider
-from .providers.kraken_provider import KrakenProvider
-from .providers.bitquery_provider import BitqueryProvider
-from .providers.coinmarketcap_provider import CoinMarketCapProvider
-from .providers.bitget_provider import BitgetProvider
-from .providers.coinbase_provider import CoinbaseProvider
-from .providers.bitcoin_provider import BitcoinProvider
-from .providers.ethereum_provider import EthereumProvider
-from .providers.solana_provider import SolanaProvider
-from .providers.sui_provider import SuiProvider
+from base_provider import BaseAPIProvider
+from coingecko_provider import CoinGeckoProvider
+from binance_provider import BinanceProvider
+from cryptocompare_provider import CryptoCompareProvider
+from kraken_provider import KrakenProvider
+from bitquery_provider import BitqueryProvider
+from coinmarketcap_provider import CoinMarketCapProvider
+from bitget_provider import BitgetProvider
+from coinbase_provider import CoinbaseProvider
+from bitcoin_provider import BitcoinProvider
+from ethereum_provider import EthereumProvider
+from solana_provider import SolanaProvider
+from sui_provider import SuiProvider
 
 # Import rate limiters
 from .rate_limiters.rate_limiter import RateLimiter
