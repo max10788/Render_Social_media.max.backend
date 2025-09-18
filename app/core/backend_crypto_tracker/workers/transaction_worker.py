0a97407d480b8bdea54cbf9425245db669e04856
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-18 22:53:38 +0200

    Update transaction_worker.py

--- app/core/backend_crypto_tracker/workers/transaction_worker.py
+++ app/core/backend_crypto_tracker/workers/transaction_worker.py
@@ -9,7 +9,9 @@ from app.core.backend_crypto_tracker.processor.blockchain_parser import Blockcha
 from app.core.backend_crypto_tracker.processor.database.models.transaction import Transaction
 from app.core.backend_crypto_tracker.processor.database.manager import DatabaseManager
 
-from app.core.backend_crypto_tracker.blockchain import EthereumProvider, SolanaProvider, SuiProvider
+from app.core.backend_crypto_tracker.blockchain.blockchain_specific.ethereum_provider import EthereumProvider
+from app.core.backend_crypto_tracker.blockchain.blockchain_specific.solana_provider import SolanaProvider
+from app.core.backend_crypto_tracker.blockchain.blockchain_specific.sui_provider import SuiProvider
 
 logger = get_logger(__name__)
 
