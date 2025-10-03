fcd998bb2f97000870249ca19596b78dda462330
max10788 <98173596+max10788@users.noreply.github.com>
2025-10-03 11:43:03 +0200

    Update init.py

--- app/core/backend_crypto_tracker/blockchain/exchanges/init.py
+++ app/core/backend_crypto_tracker/blockchain/exchanges/init.py
@@ -1,13 +1,5 @@
-"""
-API providers for fetching blockchain data.
-"""
 
-from .base_provider import BaseAPIProvider
-from .coingecko_provider import CoinGeckoProvider
-from .binance_provider import BinanceProvider
-from .cryptocompare_provider import CryptoCompareProvider
-from .kraken_provider import KrakenProvider
-from .bitquery_provider import BitqueryProvider
-from .coinmarketcap_provider import CoinMarketCapProvider
-from .bitget_provider import BitgetProvider
-from .coinbase_provider import CoinbaseProvider
+# blockchain/exchanges/__init__.py
+from .base_provider import BaseProvider, ExchangeConfig
+
+__all__ = ['BaseProvider', 'ExchangeConfig']
