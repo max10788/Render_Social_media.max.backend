0a2ff8769e0979036621745124882f1cdfa74c15
max10788 <98173596+max10788@users.noreply.github.com>
2025-10-03 12:41:04 +0200

    Update __init__.py

--- app/core/backend_crypto_tracker/blockchain/blockchain_specific/sui/__init__.py
+++ app/core/backend_crypto_tracker/blockchain/blockchain_specific/sui/__init__.py
@@ -1,3 +1,15 @@
-from .sui_provider import SuiProvider
+from .get_object_info import execute_get_object_info
+from .get_account_balance import execute_get_account_balance
+from .get_account_objects import execute_get_account_objects
+from .get_transaction import execute_get_transaction_details
+from .get_token_price import execute_get_token_price
+from .get_rate_limits import execute_get_rate_limits
 
-__all__ = ["SuiProvider"]
+__all__ = [
+    "execute_get_object_info",
+    "execute_get_account_balance", 
+    "execute_get_account_objects",
+    "execute_get_transaction_details",
+    "execute_get_token_price",
+    "execute_get_rate_limits"
+]
