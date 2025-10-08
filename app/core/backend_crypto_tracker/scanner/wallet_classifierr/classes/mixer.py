3d4dc90e8b2cbb51ac77c791c133727c9fc732d8
max10788 <98173596+max10788@users.noreply.github.com>
2025-10-08 22:05:49 +0200

    Update mixer.py

--- app/core/backend_crypto_tracker/scanner/wallet_classifierr/classes/mixer.py
+++ app/core/backend_crypto_tracker/scanner/wallet_classifierr/classes/mixer.py
@@ -3,8 +3,8 @@
 # ============================================================================
 """Mixer wallet analyzer."""
 
-from core.base_analyzer import BaseWalletAnalyzer
-from core.metric_definitions import MIXER_METRICS
+from app.core.backend_crypto_tracker.scanner.wallet_classifierr.core.base_analyzer import BaseWalletAnalyzer
+from app.core.backend_crypto_tracker.scanner.wallet_classifierr.core.metric_definitions import MIXER_METRICS
 from typing import Dict, Any
 
 
