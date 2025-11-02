7bd32e8993c274998122c8f238eea76c5d7db1bb
max10788 <98173596+max10788@users.noreply.github.com>
2025-11-02 17:27:51 +0100

    Update __init__.py

--- app/core/price_movers/api/__init__.py
+++ app/core/price_movers/api/__init__.py
@@ -1,16 +1,1 @@
-"""
-API Package
 
-Exportiert API-bezogene Module
-"""
-
-from .routes import router
-from . import dependencies
-from . import schemas
-
-
-__all__ = [
-    "router",
-    "dependencies",
-    "schemas",
-]
