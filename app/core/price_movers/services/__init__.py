fc4a24afcf2953a5c224ca5644583993e6e97625
max10788 <98173596+max10788@users.noreply.github.com>
2025-11-11 18:38:01 +0100

    Update __init__.py

--- app/core/price_movers/services/__init__.py
+++ app/core/price_movers/services/__init__.py
@@ -1,14 +1,1 @@
-"""
-Services Package
 
-Exportiert alle Service-Klassen
-"""
-
-from .analyzer import PriceMoverAnalyzer
-from .impact_calculator import ImpactCalculator
-
-
-__all__ = [
-    "PriceMoverAnalyzer",
-    "ImpactCalculator",
-]
