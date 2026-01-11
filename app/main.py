2de8ab56b45197ecd66ada5cfb65f8425c195f0a
max10788 <98173596+max10788@users.noreply.github.com>
2026-01-11 17:33:05 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect, Depends
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+from sqlalchemy.orm import Session
 import logging
 import os
 from pathlib import Path
