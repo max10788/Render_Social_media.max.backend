865f7703af2f5597a96c06dda7bc509a8b6a7f4d
max10788 <98173596+max10788@users.noreply.github.com>
2026-01-14 17:16:32 +0100

    Update network.py

--- app/core/otc_analysis/api/network.py
+++ app/core/otc_analysis/api/network.py
@@ -18,7 +18,7 @@ import logging
 from fastapi import APIRouter, HTTPException, Query, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import func, or_
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime, timedelta
 
 from .dependencies import (
